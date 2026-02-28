e1ec420bc2d8f71474e34fb55f7ed6fab9913a08
tejadasarla <dasarlateja05@gmail.com>
2026-02-28 20:47:15 +0530

    pages

--- frontend/src/components/AuthModal/AuthModal.jsx
+++ frontend/src/components/AuthModal/AuthModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { X, Mail, Lock, User, Github, Facebook, Linkedin, ArrowLeft } from 'lucide-react';
+import { X, Mail, Lock, User, Github, Linkedin, ArrowLeft } from 'lucide-react';
 import { auth } from '../../firebase.config';
 import { signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
@@ -107,14 +107,7 @@ const AuthModal = ({ isOpen, onClose, initialMode = 'login', triggerRestricted =
         }
     };
 
-    const GoogleIcon = () => (
-        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20">
-            <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
-            <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
-            <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
-            <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
-        </svg>
-    );
+
 
     if (mode === 'restricted' || mode === 'forgot') {
         return (
@@ -194,8 +187,6 @@ const AuthModal = ({ isOpen, onClose, initialMode = 'login', triggerRestricted =
                         </div>
                         <h1>Registration</h1>
                         <div className="modal-socials">
-                            <a href="#" className="social-icon"><GoogleIcon /></a>
-                            <a href="#" className="social-icon"><Facebook size={18} fill="#1877F2" color="#1877F2" /></a>
                             <a href="#" className="social-icon"><Github size={18} /></a>
                             <a href="#" className="social-icon"><Linkedin size={18} fill="#0A66C2" color="#0A66C2" /></a>
                         </div>
@@ -228,8 +219,6 @@ const AuthModal = ({ isOpen, onClose, initialMode = 'login', triggerRestricted =
                         </div>
                         <h1>Sign In</h1>
                         <div className="modal-socials">
-                            <a href="#" className="social-icon"><GoogleIcon /></a>
-                            <a href="#" className="social-icon"><Facebook size={18} fill="#1877F2" color="#1877F2" /></a>
                             <a href="#" className="social-icon"><Github size={18} /></a>
                             <a href="#" className="social-icon"><Linkedin size={18} fill="#0A66C2" color="#0A66C2" /></a>
                         </div>
